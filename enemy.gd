0dda416e6543c0a69c3e5bf111285d898f55fe42
RAnami156 <romaandreev414@gmail.com>
2025-02-05 20:53:53 +0100

    global

--- enemy.gd
+++ enemy.gd
@@ -5,10 +5,14 @@ extends CharacterBody2D
 var speed = 100
 var player_chase = false
 var player = null
+var health = 100
+var player_attack_zone = false
+var take_damage = true
 
 func _physics_process(delta: float) -> void:
+	deal_with_damage()
 	if player_chase:
-		position += (player.position - position)/speed
+		position += (player.position - position)
 		anim.play("Walk")
 		
 		if (player.position.x - position.x) < 0:
@@ -28,3 +32,31 @@ func _on_detector_body_entered(body: Node2D) -> void:
 func _on_detector_body_exited(body: Node2D) -> void:
 	player = null
 	player_chase = false
+
+func enemy():
+	pass
+	
+
+
+func _on_hitbox_body_shape_entered(body_rid: RID, body: Node2D, body_shape_index: int, local_shape_index: int) -> void:
+	if body.has_method("player"):
+		player_attack_zone = true
+
+
+func _on_hitbox_body_shape_exited(body_rid: RID, body: Node2D, body_shape_index: int, local_shape_index: int) -> void:
+	if body.has_method("player"):
+		player_attack_zone = false
+		
+func deal_with_damage():
+	if player_attack_zone and Global.player_current_attack == true:
+		if take_damage == true:
+			health -= 20
+			$damage_cooldown.start()
+			take_damage = false
+			print("slime", health)
+			if health <= 0:
+				self.queue_free()
+
+
+func _on_damage_cooldown_timeout() -> void:
+	take_damage = true
--- player.gd
+++ player.gd
@@ -1,8 +1,15 @@
 extends CharacterBody2D
 
+
+@onready var anim = $AnimatedSprite2D
 var speed = 300
 var current_dir = "none"
-@onready var anim = $AnimatedSprite2D
+var enemy_in_range = false
+var enemy_attack_cooldown = true
+var health = 100
+var player_alive = true
+var attack_ip = false
+
 
 func _ready() -> void:
 	anim.play("Front_idle")
@@ -16,7 +23,13 @@ enum {
 func _physics_process(delta):
 	#z_index = int(position.y)
 	player_movement(delta)
+	enemy_attack()
+	attack()
 	
+	if health <= 0:
+		player_alive = false
+		health = 0
+		self.queue_free()
 	
 func player_movement(delta):
 	#run
@@ -63,33 +76,80 @@ func play_anim(movement):
 		if movement == MOVING :
 			anim.play("Side_walk")
 		elif movement == IDLE:
-			anim.play("Front_idle")
+			if attack_ip == false:
+				anim.play("Front_idle")
 			
 	if dir == "left":
 		anim.flip_h = true 
 		if movement == MOVING :
 			anim.play("Side_walk")
 		elif movement == IDLE:
-			anim.play("Front_idle")
+			if attack_ip == false:
+				anim.play("Front_idle")
 	
 	if dir == "down":
 		anim.flip_h = true 
 		if movement == MOVING :
 			anim.play("Front_walk")
 		elif movement == IDLE:
-			anim.play("Idle")
+			if attack_ip == false:
+				anim.play("Idle")
 			
 	if dir == "up":
 		anim.flip_h = true 
 		if movement == MOVING :
 			anim.play("Back_walk")
 		elif movement == IDLE:
-			anim.play("Back_idle")
+			if attack_ip == false:
+				anim.play("Back_idle")
 	
+
+
+func player():
+	pass
+
+func _on_hitbox_body_entered(body: Node2D) -> void:
+	if body.has_method("enemy"):
+		enemy_in_range = true
+
+
+func _on_hitbox_body_exited(body: Node2D) -> void:
+	if body.has_method("enemy"):
+		enemy_in_range = false
+		
+func enemy_attack():
+	if enemy_in_range and enemy_attack_cooldown == true :
+		health -= 20
+		enemy_attack_cooldown = false
+		$attack_cooldown.start()
+		print(health)
+
+func _on_attack_cooldown_timeout() -> void:
+	enemy_attack_cooldown = true
+
+
 func attack():
-	if Input.is_action_just_pressed("attack"):
-		velocity.x = 0
-		velocity.y = 0
-		anim.play("Front_attack")
+	var dir = current_dir
 	
-	print(speed)
+	if Input.is_action_just_pressed("attack"):
+		Global.player_current_attack = true
+		attack_ip = true
+		if dir == "right":
+			anim.flip_h = false
+			anim.play("Side_attack")
+			$deal_attack.start()
+		if dir == "left":
+			anim.flip_h = true
+			anim.play("Side_attack")
+			$deal_attack.start()
+		if dir == "down":
+			anim.play("Back_attack")
+			$deal_attack.start()
+		if dir == "up":
+			anim.play("Front_attack")
+			$deal_attack.start()
+
+func _on_deal_attack_timeout() -> void:
+	$deal_attack.stop()
+	Global.player_current_attack = false
+	attack_ip = false
